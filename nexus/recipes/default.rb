9acf3d16fe64316b88ed08ebbe6ce55387de6d95
avani231194 <avanisinghal1@gmail.com>
2017-08-02 09:59:43 +0000

    updated

--- nexus/recipes/default.rb
+++ nexus/recipes/default.rb
@@ -6,42 +6,56 @@
 
 
 package %w(java-1.8.0-openjdk.x86_64 wget) do 
-action :install
+ action :install
 end
 
-directory "node['nexus']['dirname']" do
-action :create
+directory "#{node['nexus']['dirname']}" do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  not_if { Dir.exist?("#{node['nexus']['dirname']}") }
+   action :create
 end
+
 
 
 user 'nexus' do
-home '/home/nexus'
-shell '/bin/bash'
-action :create
+ home '/home/nexus'
+ shell '/bin/bash'
+ not_if  'grep nexus /etc/passwd', :user => 'nexus'
+ action :create
 end
+
+
 
 nexus_tar 'nexus_install' do
 title 'nexus'
 end
 
+
 template "#{node['nexus']['o_path']}/nexus.rc" do
-source 'nexus.rc.erb'
+ source 'nexus.rc.erb'
+ only_if do ! File.exist?("#{node['nexus']['o_path']}/nexus.rc" ) end
+ action :create
 end
 
 template "#{node['nexus']['path']}/nexus.vmoptions" do
-source 'nexus.vmoptions.erb'
+ source 'nexus.vmoptions.erb'
+  only_if do ! File.exist?("#{node['nexus']['path']}/nexus.vmoptions" ) end
+ action :create
 end
 
 link '/etc/init.d/nexus' do
   to '/app/nexus/bin/nexus'
-  link_type :symbolic
-  action :create
+ link_type :symbolic
+ not_if { 'test -f /app/nexus/bin/nexus' }
+ action :create
 end
 
 execute 'chkconfig' do
 command <<-EOH
-sudo chkconfig --add nexus
-sudo chkconfig --levels 345 nexus on
+ sudo chkconfig --add nexus
+ sudo chkconfig --levels 345 nexus on
 EOH
 end
 
